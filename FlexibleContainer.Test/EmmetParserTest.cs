f02e0cbaab0fd18b8825a7ac5cdb0a3a652dead5
Takumi Yamada <xirtardauq@gmail.com>
2020-03-28 10:18:21 +0900

    Add test cases for the multiplication syntax.

--- FlexibleContainer.Test/EmmetParserTest.cs
+++ FlexibleContainer.Test/EmmetParserTest.cs
@@ -259,5 +259,69 @@ namespace FlexibleContainer.Test
             var actual = ExpressionRenderer.Render("div#container>(div>p.txt1{hello})+input[type=\"button\"]{Submit}");
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Multiplication_WithSingleTag_CanParse()
+        {
+            var expected =
+                "<li></li>" +
+                "<li></li>";
+            var actual = ExpressionRenderer.Render("li*2");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Multiplication_WithSibling_CanParse()
+        {
+            var expected =
+                "<li></li><a></a>" +
+                "<li></li><a></a>";
+            var actual = ExpressionRenderer.Render("(li+a)*2");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Multiplication_WithChild_CanParse()
+        {
+            var expected =
+                "<div>" +
+                    "<li></li>" +
+                "</div>" +
+                "<div>" +
+                    "<li></li>" +
+                "</div>";
+            var actual = ExpressionRenderer.Render("(div>li)*2");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Multiplication_ParentBy_CanParse()
+        {
+            var expected =
+                "<div>" +
+                    "<li></li>" +
+                "</div>" +
+                "<div>" +
+                    "<li></li>" +
+                "</div>";
+            var actual = ExpressionRenderer.Render("div*2>li");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Multiplication_WithNestedChild_CanParse()
+        {
+            var expected =
+                "<div>" +
+                    "<div>" +
+                        "<li></li>" +
+                    "</div>" +
+                    "<div>" +
+                        "<li></li>" +
+                    "</div>" +
+                "</div>";
+            var actual = ExpressionRenderer.Render("div>(div>li)*2");
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
