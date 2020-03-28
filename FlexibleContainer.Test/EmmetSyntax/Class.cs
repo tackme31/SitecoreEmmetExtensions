f9e9e3a17d1a45b9b08b5deb2b1ce8e2a6a2cd4d
Takumi Yamada <xirtardauq@gmail.com>
2020-03-28 22:28:55 +0900

    Split the test file to each syntax.

---
+++ FlexibleContainer.Test/EmmetSyntax/Class.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FlexibleContainer.Renderer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlexibleContainer.Test.EmmetSyntax
+{
+    [TestClass]
+    public class Class
+    {
+        [TestMethod]
+        public void Class_Single_CanParse()
+        {
+            var expected = "<div class=\"class\"></div>";
+            var actual = ExpressionRenderer.Render("div.class");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Class_Multiple_CanParse()
+        {
+            var expected = "<div class=\"class2 class1\"></div>";
+            var actual = ExpressionRenderer.Render("div.class1.class2");
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ FlexibleContainer.Test/EmmetSyntax/Multiplication.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using FlexibleContainer.Renderer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlexibleContainer.Test.EmmetSyntax
+{
+    [TestClass]
+    public class Multiplication
+    {
+        [TestMethod]
+        public void Multiplication_WithSingleTag_CanParse()
+        {
+            var expected =
+                "<li></li>" +
+                "<li></li>";
+            var actual = ExpressionRenderer.Render("li*2");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Multiplication_WithSibling_CanParse()
+        {
+            var expected =
+                "<li></li><a></a>" +
+                "<li></li><a></a>";
+            var actual = ExpressionRenderer.Render("(li+a)*2");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Multiplication_WithChild_CanParse()
+        {
+            var expected =
+                "<div>" +
+                    "<li></li>" +
+                "</div>" +
+                "<div>" +
+                    "<li></li>" +
+                "</div>";
+            var actual = ExpressionRenderer.Render("(div>li)*2");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Multiplication_ParentBy_CanParse()
+        {
+            var expected =
+                "<div>" +
+                    "<li></li>" +
+                "</div>" +
+                "<div>" +
+                    "<li></li>" +
+                "</div>";
+            var actual = ExpressionRenderer.Render("div*2>li");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Multiplication_WithNestedChild_CanParse()
+        {
+            var expected =
+                "<div>" +
+                    "<div>" +
+                        "<li></li>" +
+                    "</div>" +
+                    "<div>" +
+                        "<li></li>" +
+                    "</div>" +
+                "</div>";
+            var actual = ExpressionRenderer.Render("div>(div>li)*2");
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
