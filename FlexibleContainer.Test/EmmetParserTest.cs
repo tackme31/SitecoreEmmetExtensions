5620ac31bedf51b9b4da789070c95c06ee7b737f
Takumi Yamada <xirtardauq@gmail.com>
2020-03-28 21:43:03 +0900

    Add test cases for the numbering syntax.

--- FlexibleContainer.Test/EmmetParserTest.cs
+++ FlexibleContainer.Test/EmmetParserTest.cs
@@ -323,5 +323,26 @@ namespace FlexibleContainer.Test
             var actual = ExpressionRenderer.Render("div>(div>li)*2");
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Numbering_SinlgeTag_CanParse()
+        {
+            var expected = 
+                "<h1></h1>" +
+                "<h2></h2>" +
+                "<h3></h3>";
+            var actual = ExpressionRenderer.Render("h$*3");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Numbering_MultipleReplacing_CanParse()
+        {
+            var expected =
+                "<p>001 01 0001 1</p>" +
+                "<p>002 02 0002 2</p>";
+            var actual = ExpressionRenderer.Render("p{$$$ $$ $$$$ $}*2");
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
