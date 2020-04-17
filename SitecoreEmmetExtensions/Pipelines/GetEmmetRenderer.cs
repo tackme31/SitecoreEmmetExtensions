8cbf11aa20c568e150ac4f9654caf4f42c0fe250
Takumi Yamada <xirtardauq@gmail.com>
2020-04-18 04:17:05 +0900

    Add Emmet Rendering.

---
+++ SitecoreEmmetExtensions/Pipelines/GetEmmetRenderer.cs
@@ -0,0 +1,28 @@
+﻿using Sitecore.Data;
+using Sitecore.Mvc.Pipelines.Response.GetRenderer;
+using SitecoreEmmetExtensions.Renderers;
+
+namespace SitecoreEmmetExtensions.Pipelines
+{
+    public class GetEmmetRenderer : GetRendererProcessor
+    {
+        public override void Process(GetRendererArgs args)
+        {
+            if (args.Result != null)
+            {
+                return;
+            }
+
+            var templateName = args.Rendering?.RenderingItem?.InnerItem.TemplateName;
+            if (templateName != "Emmet Rendering")
+            {
+                return;
+            }
+
+            args.Result = new EmmetRenderer()
+            {
+                Rendering = args.Rendering
+            };
+        }
+    }
+}
---
+++ SitecoreEmmetExtensions/Renderers/EmmetRenderer.cs
@@ -0,0 +1,35 @@
+﻿using Sitecore.Mvc.Common;
+using Sitecore.Mvc.Helpers;
+using Sitecore.Mvc.Presentation;
+using SitecoreEmmetExtensions.Extensions;
+using System.IO;
+using System.Web.Mvc;
+
+namespace SitecoreEmmetExtensions.Renderers
+{
+    public class EmmetRenderer : Renderer
+    {
+        public Rendering Rendering { get; set; }
+
+        public override void Render(TextWriter writer)
+        {
+            var abbreviation = Rendering?.RenderingItem?.InnerItem["Abbreviation"] ?? string.Empty;
+            if (string.IsNullOrWhiteSpace(abbreviation))
+            {
+                return;
+            }
+
+            var sitecoreHelper = GetSitecoreHelper();
+            var result = sitecoreHelper.RenderEmmetAbbreviation(abbreviation);
+            writer.Write(result);
+        }
+
+        protected virtual SitecoreHelper GetSitecoreHelper()
+        {
+            var current = ContextService.Get().GetCurrent<ViewContext>();
+            var viewDataContainer = new ViewDataContainer(current.ViewData);
+            var htmlHelper = new HtmlHelper(current, viewDataContainer);
+            return new SitecoreHelper(htmlHelper);
+        }
+    }
+}
